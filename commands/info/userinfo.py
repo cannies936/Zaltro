6a8cfb6d76ba2821c97b412b19d9beb534d358d3
cannies936 <volence02471@gmail.com>
2025-11-29 19:23:02 +0900

    userinfo.py の作成

---
+++ commands/info/userinfo.py
@@ -0,0 +1,33 @@
+@bot.tree.command(name="userinfo", description="Show user infomation")
+@app_commands.describe(user="User you want to check")
+async def userinfo(interaction: discord.Interaction, user: discord.User | None = None):
+    # ユーザーが指定されていない場合はコマンド実行者
+    user = user or interaction.user
+
+    embed = discord.Embed(
+        title=f"👤 User infomation: {user}",
+        color=discord.Color.blue()
+    )
+
+    embed.set_thumbnail(url=user.avatar.url if user.avatar else user.default_avatar.url)
+
+    embed.add_field(name="User name", value=f"`{user}`", inline=False)
+    embed.add_field(name="User ID", value=f"`{user.id}`", inline=False)
+
+    # guildに存在する場合はサーバー情報も付ける
+    if isinstance(user, discord.Member):
+        embed.add_field(name="Nick name", value=f"`{user.nick}`" if user.nick else "なし", inline=False)
+        embed.add_field(
+            name="Join date",
+            value=f"<t:{int(user.joined_at.timestamp())}:F>",
+            inline=False
+        )
+        embed.add_field(name="Role", value=", ".join([role.mention for role in user.roles if role.name != "@everyone"]), inline=False)
+
+    embed.add_field(
+        name="Account created",
+        value=f"<t:{int(user.created_at.timestamp())}:F>",
+        inline=False
+    )
+
+    await interaction.response.send_message(embed=embed)
