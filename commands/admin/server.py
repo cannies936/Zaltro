93e42b0a54073bbc0600b3dbbaa48861ec80774d
cannies936 <volence02471@gmail.com>
2025-11-29 19:11:57 +0900

    admin/server.py の作成

---
+++ commands/admin/server.py
@@ -0,0 +1,34 @@
+DEVELOPER_ID = 1362035197255749884  
+
+@bot.tree.command(name="servers", description=("check server bot joins")
+async def servers(interaction: discord.Interaction):
+
+    if interaction.user.id != DEVELOPER_ID:
+        return await interaction.response.send_message(
+            "❌ This command can be used by developer",
+            ephemeral=True)
+
+    guilds = bot.guilds
+    description = ""
+
+    for guild in guilds:
+        description += (
+            f"📌 **{guild.name}**\n"
+            f"　🆔 Server ID: `{guild.id}`\n"
+            f"　👥 Membercount: {guild.member_count}\n\n"
+        )
+
+    embed = discord.Embed(
+        title="🤖 Server joined bot",
+        description=description if description else "This bot doesn't join",
+        color=0x00aaff
+    )
+
+    await interaction.response.send_message(embed=embed)
+
+    await guild.leave()
+
+    await interaction.response.send_message(
+        f"👋 Bot left from server **{guild.name}**（ID: `{guild.id}`） ",
+        ephemeral=True
+    
