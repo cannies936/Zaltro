7868f248941f486be4542c0ef165716f8615ecd7
cannies936 <volence02471@gmail.com>
2025-12-02 20:13:42 +0900

    say.py の更新

--- commands/fun/say.py
+++ commands/fun/say.py
@@ -1,4 +1,11 @@
+class say(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
 @bot.tree.command(name="say", description="say something")
 @app_commands.describe(word="word to say")
 async def say(interaction: discord.Interaction, word: str):
     await interaction.response.send_message(word)
+
+async def setup(bot):
+    await bot.add_cog(say(bot))
