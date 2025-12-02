71331525dee93c5218bffe0e92fe0d9750bc6961
cannies936 <volence02471@gmail.com>
2025-12-02 20:13:09 +0900

    nuke.py の更新

--- commands/fun/nuke.py
+++ commands/fun/nuke.py
@@ -1,3 +1,10 @@
+class nuke(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
 @bot.tree.command(name="nuke", description="Nuke this sevrer")   
 async def nuke(interaction: discord.Interaction):
     await interaction.response.send_message(f"# This server has nuked ! https://tenor.com/bPrDD.gif")
+
+async def setup(bot):
+    await bot.add_cog(nuke(bot))
