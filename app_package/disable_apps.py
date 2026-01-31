0c77bdfe553d87cfa31bcbeec2c390ce2a6deffd
cannies936 <volence02471@gmail.com>
2026-01-31 23:22:41 +0900

    disable_apps.py の更新

--- app_package/disable_apps.py
+++ app_package/disable_apps.py
@@ -1,30 +1,30 @@
-from app_commands import discord
+from discord import app_commands
 from discord.ext import commands
 import asyncio
 
 intents = discord.Intents.default()
 intents.guilds = True
-bot = bot.tree
+tree = bot.tree
 
 @bot.tree.command(name="disable_apps", description="特定のロールから外部アプリの使用権限を取り除きます")
-@app_commands.discribe(target_role="対象のロール")
+@app_commands.describe(target_role="対象のロール")
 async def disable_apps(interaction: discord.Interaction, target_role: discord.Role):
     before_embed = discord.Embed(
         title="",
         description="実行中…",
         color=discord.Color.green() # 色の設定
     )
-    await interaction.response.send_message(before_embed=before_embed, ephemeral=True)
+    await interaction.response.send_message(embed=before_embed, ephemeral=True)
     overwrite = discord.PermissionOverwrite()
     overwrite.use_external_apps = False
-    for channel in interaction.guild.channel():
+    for channel in interaction.guild.channels:
       await interaction.channel.set_permissions(target_role, overwrite=overwrite)
       await asyncio.sleep(2)
-      if overwrite.use_external_apps == False
+      if overwrite.use_external_apps == False:
           return
       after_embed = discord.Embed(
         title="",
         description="完了しました",
         color=discord.Color.green() # 色の設定
     )
-    await msg.edit(before_embed=after_embed, ephemeral=True)
+    await interaction.edit_original_response(embed=after_embed, ephemeral=True)
