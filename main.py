b54b8ca5c094f84119a0f352d0464c734f5bfac4
cannies936 <volence02471@gmail.com>
2026-03-07 00:07:19 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import os 
 import discord
+from discord.ext import commands
+import os
+import asyncio
 
-for cog in os.listdir("app_package"):
-    if cog.endswith(".py"):
-        await bot.load_extension(f"app_package.{cog[:-3]}")
-
-# もしくはasyncioのgather(体感速度は同じ)
-asyncio.gather(*[bot.load_extension(f"app _package.{cog[:-3]}") for cog in os.listdir("app_package") if cog.endswith(".py")])
+class MyBot(commands.Bot):
+    async def setup_hook(self):
+        for cog in os.listdir("app_package"):
+            if cog.endswith(".py"):
+                await self.load_extension(f"app_package.{cog[:-3]}")
 
 if __name__ == '__main__':
     # 環境変数からトークンを取得
