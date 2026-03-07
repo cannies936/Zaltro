50c688050adecd556d6f09946362b6915db76ba4
cannies936 <volence02471@gmail.com>
2026-03-07 16:31:34 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import os
 import asyncio
 
+intents = discord.Intents.default()
+
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 class MyBot(commands.Bot):
