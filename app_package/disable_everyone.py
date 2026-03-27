b7a28097392fd89c34e2aff80ef6201560b5e877
cannies936 <volence02471@gmail.com>
2026-03-27 22:49:38 +0900

    Update disable_everyone.py

--- app_package/disable_everyone.py
+++ app_package/disable_everyone.py
@@ -13,49 +13,42 @@ class PermCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@app_commands.command(
-    name="disable_apps",description="特定のロールからeveryoneのメンション権限を取り除きます"
-)
-@app_commands.describe(target_role="対象のロール")
-async def disable_apps(self, interaction: discord.Interaction, target_role: discord.Role):
+    @app_commands.command(name="disable_apps",description="特定のロールからeveryoneのメンション権限を取り除きます")
+    @app_commands.describe(target_role="対象のロール")
+    async def disable_apps(self, interaction: discord.Interaction, target_role: discord.Role):
 
-    await interaction.response.send_message(
-        embed=discord.Embed(
-            description="実行中…",
-            color=discord.Color.green()
-        ),
-        ephemeral=True
-    )
+        await interaction.response.send_message(
+        embed=discord.Embed(description="実行中…",color=discord.Color.green()),ephemeral=True)
 
-    changed = 0
-    skipped = 0
+        changed = 0
+        skipped = 0
 
-    for channel in interaction.guild.channels:
-        current = channel.overwrites_for(target_role)
+        for channel in interaction.guild.channels:
+            current = channel.overwrites_for(target_role)
 
-        if current.mention_everyone is False:
-            skipped += 1
-            continue
+            if current.mention_everyone is False:
+                skipped += 1
+                continue
 
-        current.mention_everyone = False
+            current.mention_everyone = False
 
-        try:
-            await channel.set_permissions(target_role, overwrite=current)
-            changed += 1
-            await asyncio.sleep(0.5)
-        except Exception as e:
-            print(f"失敗: {channel.name} -> {e}")
+            try:
+                await channel.set_permissions(target_role, overwrite=current)
+                changed += 1
+                await asyncio.sleep(0.5)
+            except Exception as e:
+                print(f"失敗: {channel.name} -> {e}")
 
-    await interaction.edit_original_response(
-        embed=discord.Embed(
-            description=(
-                "完了しました\n\n"
-                f"✅ 変更: {changed} チャンネル\n"
-                f"⏭️ スキップ: {skipped} チャンネル"
-            ),
-            color=discord.Color.green()
+        await interaction.edit_original_response(
+            embed=discord.Embed(
+                description=(
+                    "完了しました\n\n"
+                    f"✅ 変更: {changed} チャンネル\n"
+                    f"⏭️ スキップ: {skipped} チャンネル"
+                ),
+                 color=discord.Color.green()
+            )
         )
-    )
 
 async def setup(bot):
-    await bot.add_cog(PermCog(bot))
+    await bot.add_cog(PermCogApp(bot))
