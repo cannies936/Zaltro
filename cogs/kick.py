e59b42fd09ddac769606acc9c05e88306b7510f9
cannies936 <volence02471@gmail.com>
2026-04-12 15:20:29 +0900

    Update kick.py

--- cogs/kick.py
+++ cogs/kick.py
@@ -12,6 +12,8 @@ class KickCog(commands.Cog):
         @app_commands.command(name="kick",description="ユーザーをサーバーからキックします")
         @app_commands.describe(user="キックするユーザー", reason="キックする理由")
         async def kick(self, interaction: discord.Interaction, user: discord.Member, reason: str)
-                    await interaction.response.send_message(
-        embed=discord.Embed(title="Kick Result:", description=f"Kick user:{display_name}", reason:color=discord.Color.green())
+                    embed=discord.Embed(title="Kick Result:", color=2AC11C)
+                    embed.add_field(name="Kick user", value="undefined", inline=False)
+                    embed.add_field(name="Reason", value="undefined", inline=False)
+                    await interaction.responce.send_message(embed=embed)
             
