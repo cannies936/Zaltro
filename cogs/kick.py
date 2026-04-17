ef5b20fa2331ed2dd5182dd93be35ed53e3670e9
cannies936 <volence02471@gmail.com>
2026-04-17 23:58:50 +0900

    Update kick.py

--- cogs/kick.py
+++ cogs/kick.py
@@ -21,14 +21,14 @@ class KickCog(commands.Cog):
             embed.add_field(name="Reason", value="{audit_reason}", inline=False)
             await interaction.response.send_message(embed=embed)
         except: Forbidden as e:
-                   embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=2AC11C)
-                   await interaction.send_message(embed=embed, ephemeral=True)
+            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
         except: HTTPEException as e:
-                   embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e}", color=2AC11C)
-                   await interaction.send_message(embed=embed, ephemeral=True)
+            embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e}", color=2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
         except: app_commands.CommandInvokeError as e:
-                   embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=2AC11C)
-                   await interaction.send_message(embed=embed, ephemeral=True)
+            embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(KickCog(bot))
