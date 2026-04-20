1db37e9dc0490dfb5a96f8584398c89f2461c637
cannies936 <volence02471@gmail.com>
2026-04-20 23:40:48 +0900

    Update ban.py

--- cogs/ban.py
+++ cogs/ban.py
@@ -9,30 +9,35 @@ tree = bot.tree
 class BanCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        @app_commands.command(name="ban",description="ユーザーをサーバーからバンします")
-        @app_commands.describe(user="バンするユーザー", reason="バンする理由", days="削除する日数")
-        async def ban(self, interaction: discord.Interaction, user: discord.User, reason: str = "理由が入力されてません", days: int)
-            try:
-                audit_reason = f"実行者: {interaction.user} | 理由: {reason}"     
-                if isinstance(user, Member):
-                    await interaction.guild.ban(user, reason=audit_reason, delete_message_days=days)
-                else:
-                  user = get_user(user)
-                  await interaction.guild.ban(user, reason=audit_reason)
-                embed = discord.Embed(title="Kick Result:", color=2AC11C)
-                embed.add_field(name="Kick user", value="{user.display_name}({user.id})", inline=False)
-                embed.add_field(name="Modertor", value="", inline=False)
+    @app_commands.command(name="ban",description="ユーザーをサーバーからバンします")
+    @app_commands.describe(user="バンするユーザー", reason="バンする理由", days="削除する日数")
+    async def ban(self, interaction: discord.Interaction, user: discord.User, reason: str = "理由が入力されてません", days: int)
+        try:
+            audit_reason = f"実行者: {interaction.user} | 理由: {reason}"     
+            if isinstance(user, Member):
+                await interaction.guild.ban(user, reason=audit_reason, delete_message_days=days)
+                embed = discord.Embed(title="Kick Result:", color=0x2AC11C)
+            　　 embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
+                embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
                 embed.add_field(name="Reason", value="{audit_reason}", inline=False)
                 await interaction.response.send_message(embed=embed)
-            except: Forbidden as e:
-                embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=2AC11C)
-                await interaction.send_message(embed=embed, ephemeral=True)
-            except: HTTPEException as e:
-                embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e}", color=2AC11C)
-                await interaction.send_message(embed=embed, ephemeral=True)
-            except: app_commands.CommandInvokeError as e:
-                embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=2AC11C)
-                await interaction.send_message(embed=embed, ephemeral=True)
+            else:
+                user = get_user(user)
+                await interaction.guild.ban(user, reason=audit_reason)
+            embed = discord.Embed(title="Kick Result:", color=0x2AC11C)
+            embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
+            embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
+            embed.add_field(name="Reason", value="{audit_reason}", inline=False)
+            await interaction.response.send_message(embed=embed)
+        except: discord.Forbidden as e:
+            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=0x2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
+        except: discord.HTTPException as e:
+            embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e}", color=2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
+        except: discord.app_commands.CommandInvokeError as e:
+            embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=0x2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(BanCog(bot))
