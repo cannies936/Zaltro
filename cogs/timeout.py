fb4a5f0bde2eab1269f4d86920858fda11ad8c3a
cannies936 <volence02471@gmail.com>
2026-05-04 00:54:41 +0900

    Update timeout.py

--- cogs/timeout.py
+++ cogs/timeout.py
@@ -12,13 +12,14 @@ class TimeoutCog(commands.Cog):
         self.bot = bot
     @app_commands.command(name="timeout",description="ユーザーをサーバーからバンします")
     @app_commands.describe(user="タイムアウトするユーザー", duration="タイムアウトする期間(指定しない場合は30秒です)", reason="タイムアウトする理由")
-    async def timeout(self, interaction: discord.Interaction, user: discord.Member, duration: datatime.timedelta = 30, reason: str = "理由が入力されてません"):
+    async def timeout(self, interaction: discord.Interaction, user: discord.Member, duration: int = 30, reason: str = "理由が入力されてません"):
         duration = timedelta(seconds=duration)
         audit_reason = f"実行者: {interaction.user} | 理由: {reason}"
         await member.timeout(duratrion, audit_reason)
         embed = discord.Embed(title="Timeout Result:", color=0x2AC11C)
+        duration = str(timedelta(seconds=duration))
         embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
-        embed.add_field(name="Duration", value=f"", inline=False)
+        embed.add_field(name="Duration", value=f"{duration}", inline=False)
         embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
         embed.add_field(name="Reason", value=f"{reason}", inline=False)
         await interaction.response.send_message(embed=embed)
