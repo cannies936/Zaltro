3a16c4a62c87983ef63d2a155e4535b30e4a6598
cannies936 <volence02471@gmail.com>
2026-05-04 11:27:50 +0900

    Implement error handling in timeout command
    Add error handling for timeout command to manage exceptions.

--- cogs/untimeout.py
+++ cogs/untimeout.py
@@ -13,11 +13,23 @@ class UntimeoutCog(commands.Cog):
     @app_commands.command(name="timeout",description="ユーザーをサーバーからバンします")
     @app_commands.describe(user="タイムアウトするユーザー", reason="タイムアウトする理由")
     async def timeout(self, interaction: discord.Interaction, user: discord.Member, reason: str = "理由が入力されてません"):
-        duration = timedelta(seconds=0)
-        audit_reason = f"実行者: {interaction.user} | 理由: {reason}"
-        await member.timeout(duratrion, audit_reason)
-        embed = discord.Embed(title="Untimeout Result:", color=0x2AC11C)
-        embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
-        embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
-        embed.add_field(name="Reason", value=f"{reason}", inline=False)
-        await interaction.response.send_message(embed=embed)
+        try:    
+            duration = timedelta(seconds=0)
+            audit_reason = f"実行者: {interaction.user} | 理由: {reason}"
+            await member.timeout(duratrion, audit_reason)
+            embed = discord.Embed(title="Untimeout Result:", color=0x2AC11C)
+            embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
+            embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
+            embed.add_field(name="Reason", value=f"{reason}", inline=False)
+            await interaction.response.send_message(embed=embed)
+       except: discord.app_commands.MissingPermissions as e:
+            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="あなたには以下の権限が不足しています:{e.missing_permissions}", color=0x2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
+        except: discord.app_commands.BotMissingPermissions as e:
+            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botには以下の権限が不足しています:{e.missing_permissions}", color=0x2AC11C)
+        except: discord.HTTPException as e:
+            embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e.code}\nError Message:{e.text}", color=0x2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
+        except: discord.app_commands.CommandInvokeError as e:
+            embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=0x2AC11C)
+            await interaction.send_message(embed=embed, ephemeral=True)
