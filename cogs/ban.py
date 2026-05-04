adacbe0b17d8b1426d3839d1be6b2f88fe25c582
cannies936 <volence02471@gmail.com>
2026-05-04 11:40:22 +0900

    Update ban.py

--- cogs/ban.py
+++ cogs/ban.py
@@ -9,6 +9,11 @@ tree = bot.tree
 class BanCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.tree.sync()
+
     @app_commands.command(name="ban",description="ユーザーをサーバーからバンします")
     @app_commands.describe(user="バンするユーザー", reason="バンする理由", days="削除する日数")
     async def ban(self, interaction: discord.Interaction, user: discord.User, reason: str = "理由が入力されてません", days: int):
