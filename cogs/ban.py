d533e7d46008742f8533c8e5d94aa298b28924a8
cannies936 <volence02471@gmail.com>
2026-05-16 13:35:57 +0900

    Update ban.py

--- cogs/ban.py
+++ cogs/ban.py
@@ -35,13 +35,13 @@ class BanCog(commands.Cog):
             embed.add_field(name="Reason", value="{audit_reason}", inline=False)
             await interaction.response.send_message(embed=embed)
         except: discord.MissingPermissions as e:
-            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=0x2AC11C)
+            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
         except: discord.HTTPException as e:
-            embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e.code}\nError Message:{e.text}", color=0x2AC11C)
+            embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e.code}\nError Message:{e.text}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
         except: discord.app_commands.CommandInvokeError as e:
-            embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=0x2AC11C)
+            embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
