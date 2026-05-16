71a73c8f4246b1018fae569b61d0780820186d1f
cannies936 <volence02471@gmail.com>
2026-05-16 18:00:46 +0900

    Update embed title and footer in dice command

---
+++ cogs/supurite.py
@@ -0,0 +1,29 @@
+import discord
+import discord from app_commands
+from discord.ext import commands
+import asyncio
+import random
+
+bot = commands.Bot(command_prefix="/", intents=intents) 
+tree = bot.tree
+
+class DiceCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.tree.sync()
+    
+    @app_commands.command(name="supurite",description="ランダムなスプライト画像を送信します")
+    async def dice(self, interaction: discord.Interaction):
+      try:
+          files = [discord.File("images/sprite_bottle_1.png", filename="sprite_bottle_1.png"), discord.File("images/sprite_can_1.png", filename="sprite_can_1.png"), discord.File("images/sprite_image_3.jpg", filename="sprite_image_3.jpg")]
+          sprite_file = random.choice(file)
+          embed = discord.Embed(title="🥤 Sprite Random!", description="ランダムに選ばれたSprite画像です！")
+          embed.set_image(url=f"attachment://{sprite_file}")
+          embed.set_footer(text=f"要求者: {interaction.user.display_name}, icon_url=interaction.user.display_avatar.url)
+          await interaction.response(embed=embed)
+      except app_commands.CommandInvokeError as e:
+          embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
+          await interaction.send_message(embed=embed, ephemeral=True)
