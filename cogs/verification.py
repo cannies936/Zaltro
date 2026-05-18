f4c7031ae515088db188d37a3acce78cd014448e
cannies936 <volence02471@gmail.com>
2026-05-19 00:24:13 +0900

    verification.py の更新

--- cogs/verification.py
+++ cogs/verification.py
@@ -7,3 +7,7 @@ from captcha.image import ImageCaptcha
 import asyncio
 import pillow
 from discord import ui
+
+class AuthView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
