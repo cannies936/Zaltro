a37a26f95c856a90e3bca49bb8a51e7c4512b192
cannies936 <volence02471@gmail.com>
2026-05-20 00:06:00 +0900

    Update verification.py

--- cogs/verification.py
+++ cogs/verification.py
@@ -11,3 +11,6 @@ from discord import ui
 class AuthView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+    @ui.button(label="認証", style=discord.ButtonStyle.success)
+    async def success_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("ボタンが押されました！", ephemeral=True)
