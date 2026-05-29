310094bb9453a5509c91e1016e7b12927d408a94
cannies936 <volence02471@gmail.com>
2026-05-29 18:36:04 +0900

    Update supurite.py

--- cogs/supurite.py
+++ cogs/supurite.py
@@ -16,6 +16,7 @@ class SupuriteCog(commands.Cog):
         await self.bot.tree.sync()
     
     @app_commands.command(name="supurite",description="ランダムなスプライト画像を送信します")
+    @app_commands.Cooldown(2, 60)
     async def supurite(self, interaction: discord.Interaction):
       try:
           files = [discord.File("images/sprite_bottle_1.png", filename="sprite_bottle_1.png"), discord.File("images/sprite_can_1.png", filename="sprite_can_1.png"), discord.File("images/sprite_image_3.jpg", filename="sprite_image_3.jpg")]
@@ -27,6 +28,8 @@ class SupuriteCog(commands.Cog):
       except app_commands.CommandInvokeError as e:
           embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
           await interaction.responce.send_message(embed=embed, ephemeral=True)
+      except app_commands.CommandOnCooldown as e:
+          embed = discord.Embed(title="実行に失敗しました", description="実行してから1分間程度は使えません", color=discord.Colour.red())
 
 async def setup(bot):
     await bot.add_cog(SupuriteCog(bot))
