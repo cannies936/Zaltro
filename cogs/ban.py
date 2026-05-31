19d5e02ffaa45bbf0f9d85a2ddd283afb8c353ea
cannies936 <volence02471@gmail.com>
2026-05-31 12:21:19 +0900

    Update ban.py

--- cogs/ban.py
+++ cogs/ban.py
@@ -1,8 +1,10 @@
 import discord
-import discord from app_commands
+from app_commands import discord
 from discord.ext import commands
 import asyncio
 
+intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix="/", intents=intents) 
 tree = bot.tree
 
@@ -16,31 +18,31 @@ class BanCog(commands.Cog):
 
     @app_commands.command(name="ban",description="ユーザーをサーバーからバンします")
     @app_commands.describe(user="バンするユーザー", reason="バンする理由", days="削除する日数")
-    async def ban(self, interaction: discord.Interaction, user: discord.User, reason: str = "理由が入力されてません", days: int):
+    async def ban(self, interaction: discord.Interaction, user: discord.User, days: int, reason: str = "理由が入力されてません"):
         try:
             audit_reason = f"実行者: {interaction.user} | 理由: {reason}"     
-            if isinstance(user, Member):
+            if isinstance(user, discord.Member):
                 await interaction.guild.ban(user, reason=audit_reason, delete_message_days=days)
                 embed = discord.Embed(title="Ban Result:", color=0x2AC11C)
             　　 embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
                 embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
-                embed.add_field(name="Reason", value="{audit_reason}", inline=False)
+                embed.add_field(name="Reason", value=f"{audit_reason}", inline=False)
                 await interaction.response.send_message(embed=embed)
             else:
-                user = get_user(user)
+                user = self.bot.fetch_user(user)
                 await interaction.guild.ban(user, reason=audit_reason)
             embed = discord.Embed(title="Ban Result:", color=0x2AC11C)
             embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
             embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
-            embed.add_field(name="Reason", value="{audit_reason}", inline=False)
+            embed.add_field(name="Reason", value=f"{audit_reason}", inline=False)
             await interaction.response.send_message(embed=embed)
-        except: discord.MissingPermissions as e:
-            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=discord.Colour.red())
+        except discord.MissingPermissions as e:
+            embed = discord.Embed(title="実行に失敗しました", description="Botの権限もしくは自身の権限を確認してください:{e}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
-        except: discord.HTTPException as e:
+        except discord.HTTPException as e:
             embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e.code}\nError Message:{e.text}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
-        except: discord.app_commands.CommandInvokeError as e:
+        except discord.app_commands.CommandInvokeError as e:
             embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
 
