ec1234a2909faf7e38663eb41dcca7abe5fd6aaf
cannies936 <volence02471@gmail.com>
2026-06-09 00:26:54 +0900

    Fix syntax error in kick.py

--- cogs/kick.py
+++ cogs/kick.py
@@ -21,22 +21,22 @@ class KickCog(commands.Cog):
         audit_reason = f"実行者: {interaction.user} | 理由: {reason}"     
         try:
             await interaction.guild.kick(user, reason=audit_reason)
-            embed=discord.Embed(title="Kick Result:", color=0x2AC11C)
+            embed = discord.Embed(title="Kick Result:", color=0x2AC11C)
             embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
             embed.add_field(name="Modertor", value="{interaction.user}", inline=False)
             embed.add_field(name="Reason", value="{audit_reason}", inline=False)
             await interaction.response.send_message(embed=embed)
         except discord.app_commands.MissingPermissions as e:
-            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="あなたには以下の権限が不足しています:{e.missing_permissions}", color=discord.Colour.red())
+            embed = discord.Embed(title="実行に失敗しました", description="あなたには以下の権限が不足しています:{e.missing_permissions}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
         except discord.app_commands.BotMissingPermissions as e:
-            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botには以下の権限が不足しています:{e.missing_permissions}", embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
-            await interaction.send_message(embed=embed, ephemeral=True))
+            embed = discord.Embed(title="実行に失敗しました", description="Botには以下の権限が不足しています:{e.missing_permissions}", color=discord.Colour.red())
+            await interaction.send_message(embed=embed, ephemeral=True)
         except discord.HTTPException as e:
             embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e.code}\nError Message:{e.text}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
         except discord.app_commands.CommandInvokeError as e:
-            embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
+            embed = discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
