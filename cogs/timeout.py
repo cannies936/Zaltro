d24ec890bcf9d7373a76be95178d6a5eb81fa2d6
cannies936 <volence02471@gmail.com>
2026-06-10 00:15:50 +0900

    Update timeout.py

--- cogs/timeout.py
+++ cogs/timeout.py
@@ -29,15 +29,15 @@ class TimeoutCog(commands.Cog):
             embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
             embed.add_field(name="Reason", value=f"{reason}", inline=False)
             await interaction.response.send_message(embed=embed)
-        except: discord.app_commands.MissingPermissions as e:
-            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="あなたには以下の権限が不足しています:{e.missing_permissions}", color=discord.Colour.red())
+        except app_commands.MissingPermissions:
+            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="あなたには以下の権限が不足しています:メンバーをタイムアウト", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
-        except: discord.app_commands.BotMissingPermissions as e:
-            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botには以下の権限が不足しています:{e.missing_permissions}", color=discord.Colour.red())
-        except: discord.HTTPException as e:
+        except app_commands.BotMissingPermissions:
+            embed = discord.embed=discord.Embed(title="実行に失敗しました", description="Botには以下の権限が不足しています:メンバーをタイムアウト", color=discord.Colour.red())
+        except discord.HTTPException as e:
             embed = discord.Embed(title="実行に失敗しました", description="Error Code:{e.code}\nError Message:{e.text}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
-        except: discord.app_commands.CommandInvokeError as e:
+        except app_commands.CommandInvokeError as e:
             embed = embed=discord.Embed(title="実行に失敗しました", description="コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
             await interaction.send_message(embed=embed, ephemeral=True)
 
