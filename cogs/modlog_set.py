cb1b42b24d62110727ffa0a51d4ae4bd2b105552
cannies936 <volence02471@gmail.com>
2026-06-10 00:10:05 +0900

    Update modlog_set.py

--- cogs/modlog_set.py
+++ cogs/modlog_set.py
@@ -1,9 +1,10 @@
 import discord
-from app_commands import discord
+from discord import app_commands
 from discord.ext import commands
 import asyncio
 from timedate import timedelta
 
+intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents) 
 tree = bot.tree
 
