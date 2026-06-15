43abbaa8b917d142bc9a5138773660cb23dfc58c
cannies936 <volence02471@gmail.com>
2026-06-16 00:06:49 +0900

    Update kick.py

--- cogs/kick.py
+++ cogs/kick.py
@@ -3,10 +3,6 @@ from discord import app_commands
 from discord.ext import commands
 import asyncio
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="/", intents=intents) 
-tree = bot.tree
-
 class KickCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
