19b42642f81359e33533b0142fb994a1e898598f
cannies936 <volence02471@gmail.com>
2026-06-27 11:28:08 +0900

    Update timeout.py

--- cogs/timeout.py
+++ cogs/timeout.py
@@ -23,7 +23,7 @@ class TimeoutCog(commands.Cog):
             await user.timeout(duration, reason=audit_reason)
             embed = discord.Embed(title="Timeout Result:", color=0x2AC11C)
             embed.add_field(name="Target", value=f"{user.display_name}({user.id})", inline=False)
-            embed.add_field(name="Duration", value="str(duration)", inline=False)
+            embed.add_field(name="Duration", value=str(duration), inline=False)
             embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
             embed.add_field(name="Reason", value=f"{reason}", inline=False)
             await interaction.response.send_message(embed=embed)
