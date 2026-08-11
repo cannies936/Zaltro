0e776c3f42ec73788ebe67b54f95040a970a6ab8
cannies936 <volence02471@gmail.com>
2026-08-11 18:56:15 +0900

    エラー修正

--- cogs/purge.py
+++ cogs/purge.py
@@ -12,15 +12,13 @@ class PurgeCog(commands.Cog):
         await self.bot.tree.sync()
 
     @app_commands.command(name="purge",description="メッセージを一括削除します")
-    @app_commands.describe(amount="一括削除するメッセージ数", reason="一括削除した理由", ephemeral="表示するかどうか")
-    @app_commands.checks.has_permissions(manage_messages=True)
-    @app_commands.checks.has_permissions(read_message_history=True)
-    @app_commands.checks.bot_has_permissions(manage_messages=True)
-    @app_commands.checks.bot_has_permissions(read_message_history=True)
-    async def purge(self, interaction: discord.Interaction, amount: app_commands.Range[int, 1, 100], reason: str = "理由が入力されてません", ephemeral: bool = True):
+    @app_commands.describe(amount="一括削除するメッセージ数", user="対象のユーザー", reason="一括削除した理由", ephemeral="表示するかどうか")
+    @app_commands.checks.has_permissions(manage_messages=True, read_message_history=True)
+    @app_commands.checks.bot_has_permissions(manage_messages=True, read_message_history=True)
+    async def purge(self, interaction: discord.Interaction, amount: app_commands.Range[int, 1, 100], user: discord.Member = None, reason: str = "理由が入力されてません", ephemeral: bool = True):
         try:
             audit_reason = f"実行者: {interaction.user} | 理由: {reason}"     
-            await interaction.channel.purge(limit=amount, reason=audit_reason, delete_message_days=days)
+            await interaction.channel.purge(limit=amount, check=lambda message: message.author.id == user.id, reason=audit_reason)
             embed = discord.Embed(title="Purge Result:", color=0x2AC11C)
             embed.add_field(name="Channel", value=f"{interaction.channel}", inline=False)
             embed.add_field(name="Amount", value=f"{amount}", inline=False)
@@ -29,10 +27,9 @@ class PurgeCog(commands.Cog):
             await interaction.response.send_message(embed=embed, ephemeral=ephemeral)
         except app_commands.MissingPermissions:
             embed = discord.Embed(title="実行に失敗しました", description="あなたには以下の権限が不足しています:メッセージの管理、メッセージ履歴を読む", color=discord.Colour.red())
-            await interaction.send_message(embed=embed, ephemeral=True)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
         except app_commands.BotMissingPermissions:
             embed = discord.Embed(title="実行に失敗しました", description="Botには以下の権限が不足しています:メッセージの管理、メッセージ履歴を読む", color=discord.Colour.red())
-            await interaction.send_message(embed=embed, ephemeral=True)
             await interaction.response.send_message(embed=embed, ephemeral=True)
         except discord.HTTPException as e:
             embed = discord.Embed(title="実行に失敗しました", description=f"Error Code:{e.code}\nError Message:{e.text}", color=discord.Colour.red())
