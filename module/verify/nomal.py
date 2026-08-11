14682d13aced18d1a0f03a6b5349bf6503bbb756
cannies936 <volence02471@gmail.com>
2026-08-11 19:01:22 +0900

    Update role assignment with reason in interaction
    Add reason for role assignment in button interaction.

--- module/verify/nomal.py
+++ module/verify/nomal.py
@@ -8,7 +8,7 @@ class NomalView(discord.ui.View):
     @discord.ui.button(label="認証する", style=discord.ButtonStyle.green)
     async def my_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
-      　    interaction.user.add_roles(role)
+      　    interaction.user.add_roles(role, reason="Zaltro即時認証")
             embed = discord.Embed(title="", description="✅認証しました", color=discord.Color.green())
             await interaction.response.send_message(embed=embed, ephemeral=True)
         except Forbidden:
