6ac1e791fd831323850273f2a5bbaab191af30ac
cannies936 <volence02471@gmail.com>
2026-08-14 23:58:58 +0900

    一旦保存

--- module/verify/image.py
+++ module/verify/image.py
@@ -3,3 +3,12 @@ import asyncio
 from captcha.image import ImageCaptcha
 import random
 import string
+
+captcha_image = ImageCaptcha(width=280, height=90)
+
+def code_generate():
+    code_source = string.ascii_letters + string.digits
+    return "".join(random.choice(code_source) for _ in range(7))
+
+
+
