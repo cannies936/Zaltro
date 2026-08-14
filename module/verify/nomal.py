0f29398e0e854729b45c6eb589f3e626bb5d0de6
cannies936 <volence02471@gmail.com>
2026-08-15 08:47:25 +0900

    Update nomal.py

--- module/verify/nomal.py
+++ module/verify/nomal.py
@@ -2,15 +2,19 @@ import discord
 import asyncio
 
 class NomalView(discord.ui.View):
-    def __init__(self, role):
+    def __init__(self, role: discord.Role):
         super().__init__(timeout=0)  # タイムアウト時間（秒）
         self.role = role
     @discord.ui.button(label="認証する", style=discord.ButtonStyle.green, custom_id=nomal)
     async def nomal(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
-      　    interaction.user.add_roles(role, reason="Zaltro即時認証")
-            embed = discord.Embed(title="", description="✅認証しました", color=discord.Color.green())
+            if role in interaction.user.roles:
+                embed = discord.Embed(title="❌エラー", description="認証に失敗しました: 既に認証済みです", color=discord.Color.red())
+                await interaction.response.send_message(embed=embed, ephemeral=True)
             await interaction.response.send_message(embed=embed, ephemeral=True)
+                interaction.user.add_roles(role, reason="Zaltro即時認証")
+                embed = discord.Embed(title="", description="✅認証しました", color=discord.Color.green())
+                await interaction.response.send_message(embed=embed, ephemeral=True)
         except Forbidden:
             embed = discord.Embed(title="❌エラー", description="認証に失敗しました: BOTに適切な権限がないかロールがBOTよりも上にあります", color=discord.Color.red())
             await interaction.response.send_message(embed=embed, ephemeral=True)
