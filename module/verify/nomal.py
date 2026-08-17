34e66e645810adc275fa166b8d58c257fcbc8a3b
cannies936 <volence02471@gmail.com>
2026-08-17 20:01:29 +0900

    Correct role assignment in NomalView
    Fix role reference in NomalView class for role assignment.

--- module/verify/nomal.py
+++ module/verify/nomal.py
@@ -3,8 +3,8 @@ import asyncio
 
 class NomalView(discord.ui.View):
     def __init__(self, role: discord.Role):
-        super().__init__(timeout=0)  # タイムアウト時間（秒）
         self.role = role
+        super().__init__(timeout=0)
     @discord.ui.button(label="認証する", style=discord.ButtonStyle.green, custom_id=nomal)
     async def nomal(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
@@ -12,7 +12,7 @@ class NomalView(discord.ui.View):
                 embed = discord.Embed(title="❌エラー", description="認証に失敗しました: 既に認証済みです", color=discord.Color.red())
                 await interaction.response.send_message(embed=embed, ephemeral=True)
             await interaction.response.send_message(embed=embed, ephemeral=True)
-                interaction.user.add_roles(role, reason="Zaltro即時認証")
+                interaction.user.add_roles(self.role, reason="Zaltro即時認証")
                 embed = discord.Embed(title="", description="✅認証しました", color=discord.Color.green())
                 await interaction.response.send_message(embed=embed, ephemeral=True)
         except Forbidden:
