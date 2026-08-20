0e7daf0e669beae5e04f9469a84ab250b6b7af4b
cannies936 <volence02471@gmail.com>
2026-08-21 00:01:59 +0900

    Update purge.py

--- cogs/purge.py
+++ cogs/purge.py
@@ -17,6 +17,7 @@ class PurgeCog(commands.Cog):
     @app_commands.checks.bot_has_permissions(manage_messages=True, read_message_history=True)
     async def purge(self, interaction: discord.Interaction, amount: app_commands.Range[int, 1, 100], user: discord.Member = None, reason: str = "理由が入力されてません", ephemeral: bool = True):
         try:
+            await interaction.response.defer()
             audit_reason = f"実行者: {interaction.user} | 理由: {reason}"     
             await interaction.channel.purge(limit=amount, check=lambda message: message.author.id == user.id, reason=audit_reason)
             embed = discord.Embed(title="Purge Result:", color=0x2AC11C)
@@ -24,19 +25,19 @@ class PurgeCog(commands.Cog):
             embed.add_field(name="Amount", value=f"{amount}", inline=False)
             embed.add_field(name="Modertor", value=f"{interaction.user}", inline=False)
             embed.add_field(name="Reason", value=f"{reason}", inline=False)
-            await interaction.response.send_message(embed=embed, ephemeral=ephemeral)
+            await interaction.response.followup.send(embed=embed, ephemeral=ephemeral)
         except app_commands.MissingPermissions:
             embed = discord.Embed(title="実行に失敗しました", description="あなたには以下の権限が不足しています:メッセージの管理、メッセージ履歴を読む", color=discord.Colour.red())
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.response.followup.send(embed=embed, ephemeral=True)
         except app_commands.BotMissingPermissions:
             embed = discord.Embed(title="実行に失敗しました", description="Botには以下の権限が不足しています:メッセージの管理、メッセージ履歴を読む", color=discord.Colour.red())
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.response.followup.send(embed=embed, ephemeral=True)
         except discord.HTTPException as e:
             embed = discord.Embed(title="実行に失敗しました", description=f"Error Code:{e.code}\nError Message:{e.text}", color=discord.Colour.red())
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.response.followup.send(embed=embed, ephemeral=True)
         except app_commands.CommandInvokeError as e:
             embed = embed=discord.Embed(title="実行に失敗しました", description=f"コマンド実行中にエラーが発生しました:{e}", color=discord.Colour.red())
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.response.followup.send(embed=embed, ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(PurgeCog(bot))
