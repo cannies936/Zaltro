f11bd0744ee7c7e3a869b300431eed58eae634a3
cannies936 <volence02471@gmail.com>
2026-08-21 00:12:23 +0900

    Fix role check and button custom_id in NomalView

--- module/verify/nomal.py
+++ module/verify/nomal.py
@@ -5,16 +5,16 @@ class NomalView(discord.ui.View):
     def __init__(self, role: discord.Role):
         self.role = role
         super().__init__(timeout=0)
-    @discord.ui.button(label="認証する", style=discord.ButtonStyle.green, custom_id=nomal)
+    @discord.ui.button(label="認証する", style=discord.ButtonStyle.green, custom_id="nomal")
     async def nomal(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
-            if role in interaction.user.roles:
+            if self.role in interaction.user.roles:
                 embed = discord.Embed(title="❌エラー", description="認証に失敗しました: 既に認証済みです", color=discord.Color.red())
                 await interaction.response.send_message(embed=embed, ephemeral=True)
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            else:
                 interaction.user.add_roles(self.role, reason="Zaltro即時認証")
                 embed = discord.Embed(title="", description="✅認証しました", color=discord.Color.green())
                 await interaction.response.send_message(embed=embed, ephemeral=True)
-        except Forbidden:
+        except discord.Forbidden:
             embed = discord.Embed(title="❌エラー", description="認証に失敗しました: BOTに適切な権限がないかロールがBOTよりも上にあります", color=discord.Color.red())
             await interaction.response.send_message(embed=embed, ephemeral=True)
