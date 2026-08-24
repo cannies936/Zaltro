d08d2a604149610275f908951ef86768e0bb4c70
cannies936 <volence02471@gmail.com>
2026-08-24 22:12:54 +0900

    修正

--- module/verify/image.py
+++ module/verify/image.py
@@ -11,13 +11,6 @@ def code_generate():
     chars = string.ascii_letters + string.digits
     return "".join(random.choice(chars) for _ in range(7))
 
-class CaptchaSelect(discord.ui.Select):
-    def __init__(self, role: discord.Role, captcha_code: str, captcha_source: list[str]):
-        self.role = role
-        self.captcha_code = captcha_code
-        options = [discord.SelectOption(label=code)for code in captcha_source]
-        super().__init__(placeholder="画像に書かれた文字を選択してください", min_values=1, max_values=1, options=options, custom_id="captcha_image")
-
 class ImageView(discord.ui.View):
     def __init__(self, role: discord.Role):
         self.role = role
@@ -31,24 +24,24 @@ class ImageView(discord.ui.View):
         image_bytes = io.BytesIO(image.read())
         file = discord.File(fp=image_bytes, filename="captcha.png")
         select_menu = discord.ui.Select(placeholder="画像に書かれた文字を選択してください", min_values=1, max_values=1, options=[discord.SelectOption(label=option, value=option) for option in captcha_source], custom_id="captcha_image")
-    async def select_callback(select_interaction: discord.Interaction):
-        choice = select_menu.values[0]
-        if choice == captcha_code:
-            try:
-      　        if self.role in select_interaction.user.roles:
-                    embed = discord.Embed(title="❌エラー", description="認証に失敗しました: 既に認証済みです", color=discord.Color.red())
-                    await interaction.response.send_message(embed=embed, ephemeral=True) 
-                else:
-                    await select_interaction.user.add_roles(self.role, reason="Zaltro画像認証")
-                    embed = discord.Embed(title="", description="✅認証しました", color=discord.Color.green())
-                    await interaction.response.send_message(embed=embed, ephemeral=True)
-            except discord.Forbidden:
-                embed = discord.Embed(title="❌エラー", description="認証に失敗しました: BOTに適切な権限がないかロールがBOTよりも上にあります", color=discord.Color.red())
-                await select_interaction.response.send_message(embed=embed, ephemeral=True)
-        select_menu.callback = select_callback
-        view = discord.ui.View(timeout=300)
-        view.add_item(select_menu)
-        embed = discord.Embed(title="", description="以下の指示に従ってください", color=discord.Color.green())
-        embed.set_image(url="attachment://captcha.png")
-        await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
+        async def select_callback(select_interaction: discord.Interaction):
+            choice = select_menu.values[0]
+            if choice == captcha_code:
+                try:
+      　            if self.role in select_interaction.user.roles:
+                        embed = discord.Embed(title="❌エラー", description="認証に失敗しました: 既に認証済みです", color=discord.Color.red())
+                        await select_interaction.response.send_message(embed=embed, ephemeral=True) 
+                    else:
+                        await select_interaction.user.add_roles(self.role, reason="Zaltro画像認証")
+                        embed = discord.Embed(title="", description="✅認証しました", color=discord.Color.green())
+                        await select_interaction.response.send_message(embed=embed, ephemeral=True)
+                except discord.Forbidden:
+                    embed = discord.Embed(title="❌エラー", description="認証に失敗しました: BOTに適切な権限がないかロールがBOTよりも上にあります", color=discord.Color.red())
+                    await select_interaction.response.send_message(embed=embed, ephemeral=True)
+            select_menu.callback = select_callback
+            view = discord.ui.View(timeout=0)
+            view.add_item(select_menu)
+            embed = discord.Embed(title="", description="以下の指示に従ってください", color=discord.Color.green())
+            embed.set_image(url="attachment://captcha.png")
+            await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
 
