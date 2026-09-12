14102c43c22f38cd8ab16ea0deebf2700a49e0de
cannies936 <volence02471@gmail.com>
2026-09-13 00:21:22 +0900

    Enhance userinfo command with response deferral
    Defer interaction response and update user avatar retrieval.

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -10,4 +10,9 @@ class UserinfoCog(commands.Cog):
     @app_commands.command(name="userinfo",description="ユーザー情報を取得します")
     @app_commands.describe(user="調べたいユーザー(値がない場合は自分自身)")
     async def userinfo(self, interaction: discord.Interaction, user: discord.User = None):
-      
+        await interaction.response.defer()
+        if user is None:
+            user = interaction.user
+        embed = discord.Embed(title="👤 ユーザー情報", color=0x2AC11C)
+        embed.set_thumbnail(url=user.display_avatar.url)
+        embed.add_field(name="", value="", inline=True)
