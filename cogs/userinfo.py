fcbb4a0c938d50755860abc8038b245796e7091a
cannies936 <volence02471@gmail.com>
2026-09-15 00:20:43 +0900

    保存

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -13,12 +13,12 @@ class UserinfoCog(commands.Cog):
         await interaction.response.defer()
         if user is None:
             user = interaction.user
+        if 
+        
         embed = discord.Embed(title="👤 ユーザー情報", color=0x2AC11C)
         embed.set_thumbnail(url=user.display_avatar.url)
         embed.add_field(name="📛 ユーザー名", value=f"{user.name}", inline=True)
         embed.add_field(name="🆔 ユーザーID", value=f"{user.id}", inline=True)
         embed.add_field(name="📝 ニックネーム", value=f"{user.display_name}", inline=True)
         embed.add_field(name="📅 アカウント作成日", value=f"{user.created_at.strftime("%Y年%m月%d日 %H:%M")}", inline=True)
-
-
-
+        embed.add_field(name="📶 ステータス", value=f"{status_set}", inline=True)
