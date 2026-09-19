0686353cbff3c78c0d6e4621069b68c2cd029f9d
cannies936 <volence02471@gmail.com>
2026-09-19 12:26:25 +0900

    Enhance userinfo command with status and device info
    Updated userinfo command to display user status and device.

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -13,12 +13,36 @@ class UserinfoCog(commands.Cog):
         await interaction.response.defer()
         if user is None:
             user = interaction.user
-        if 
-        
-        embed = discord.Embed(title="👤 ユーザー情報", color=0x2AC11C)
+
+        embed = discord.Embed(title=f"👤 {user.global_name}のユーザー情報", color=0x2AC11C)
         embed.set_thumbnail(url=user.display_avatar.url)
         embed.add_field(name="📛 ユーザー名", value=f"{user.name}", inline=True)
         embed.add_field(name="🆔 ユーザーID", value=f"{user.id}", inline=True)
         embed.add_field(name="📝 ニックネーム", value=f"{user.display_name}", inline=True)
         embed.add_field(name="📅 アカウント作成日", value=f"{user.created_at.strftime("%Y年%m月%d日 %H:%M")}", inline=True)
+        if isinstance(user, discord.Member) == True:
+            if user.status == discord.Status.online:
+                user_status = "🟢 オンライン"
+                if user.is_on_mobile == True:
+                    user_device = "📱 モバイル"
+                else:
+                    user_device = "🌐・💻 Web/デスクトップ"
+            elif user.status == discord.Status.idle:
+                user_status = "🌙 退席中"
+                if user.is_on_mobile == True:
+                    user_device = "📱 モバイル"
+                else:
+                    user_device = "🌐・💻 Web/デスクトップ"
+            elif user.status == discord.Status.idle:
+                user_status = "⛔️ 取り込み中"
+                if user.is_on_mobile == True:
+                    user_device = "📱 モバイル"
+                else:
+                    user_device = "🌐・💻 Web/デスクトップ"
+            elif user.status == discord.Status.offline:
+                user_status = "🔘 オフライン"
+                user_device = "❓ 不明"
+        else:
+            pass
+
         embed.add_field(name="📶 ステータス", value=f"{status_set}", inline=True)
